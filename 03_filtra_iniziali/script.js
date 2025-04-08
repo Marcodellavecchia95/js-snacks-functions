e858c18f4185ecf12ae151c8326b15a7bd0217e7
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-04-08 15:25:02 +0200

    add "filtra-iniziali" js

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,21 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
-
+let newArray = [];
 // Dichiara la funzione qui.
-
+function findNamesLetter(namesArray, toFindLetter) {
+  let newNamesList = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    let name = namesArray[i];
+    if (toFindLetter === name[0]) {
+      newNamesList.push(name);
+    }
+  }
+  return newNamesList;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(findNamesLetter(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
